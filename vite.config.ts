f2c11c175267b4b98ee2bf9c73dbc5bc7c5f0227
Romain Durand <romain.durand@radiofrance.com>
2026-03-15 20:40:15 +0100

    fix: tensorflow (commonjs) as external

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	ssr: {
+		external: ['@tensorflow/tfjs']
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
